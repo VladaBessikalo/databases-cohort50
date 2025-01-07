946005b84b724416a33fd3a8f15616259ef9acf7
Vlada B <vlada.bessikalo@gmail.com>
2025-01-07 13:00:31 +0100

    complete exercise-1

---
+++ Week2/assignment-w2/exercise1-keys.js
@@ -0,0 +1,42 @@
+import mysql from 'mysql2/promise';
+
+const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'hyfuser',
+    password: 'hyfpassword'
+});
+
+async function setupDatabase() {
+    const createDatabase = `CREATE DATABASE IF NOT EXISTS academics`;
+    const useDatabase = `USE academics`;
+    const createAuthorsTable = `
+        CREATE TABLE IF NOT EXISTS authors (
+        author_id INT PRIMARY KEY, 
+        author_name VARCHAR(100) NOT NULL, 
+        university VARCHAR(100), 
+        date_of_birth DATE, 
+        h_index INT DEFAULT 0, 
+        gender ENUM('m', 'f')
+    )`;
+    const addColumnToAuthorsTable = `
+        ALTER TABLE authors
+        ADD COLUMN mentor INT,
+        ADD CONSTRAINT fk_mentor FOREIGN KEY (mentor) REFERENCES authors(author_id)
+    `;
+
+    try {
+        await connection.connect();
+        console.log('Connected!');
+        await connection.query(createDatabase);
+        await connection.query(useDatabase);
+        await connection.query(createAuthorsTable);
+        await connection.query(addColumnToAuthorsTable);
+    } catch (error) {
+        console.error(error);
+        connection.end();
+    }
+
+    connection.end();
+}
+
+setupDatabase();
